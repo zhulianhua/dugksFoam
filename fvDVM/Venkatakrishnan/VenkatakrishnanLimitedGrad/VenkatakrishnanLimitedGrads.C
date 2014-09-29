4db9d2207df7d8bfd2e4cd7f02a92499512887a2
zhulianhua <zhulianhua121@gmail.com>
2014-09-29 20:53:53 +0800

    add Venkatakrishnan

---
+++ fvDVM/Venkatakrishnan/VenkatakrishnanLimitedGrad/VenkatakrishnanLimitedGrads.C
@@ -0,0 +1,189 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "VenkatakrishnanLimitedGrad.H"
+#include "gaussGrad.H"
+#include "fvMesh.H"
+#include "volMesh.H"
+#include "surfaceMesh.H"
+#include "volFields.H"
+#include "fixedValueFvPatchFields.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+namespace fv
+{
+    makeFvGradScheme(VenkatakrishnanLimitedGrad)
+}
+}
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+template<>
+Foam::tmp<Foam::volVectorField>
+Foam::fv::VenkatakrishnanLimitedGrad<Foam::scalar>::calcGrad
+(
+    const volScalarField& vsf,
+    const word& name
+) const
+{
+    const fvMesh& mesh = vsf.mesh();
+
+    tmp<volVectorField> tGrad = basicGradScheme_().calcGrad(vsf, name);
+
+    if (k_ < SMALL)
+    {
+        return tGrad;
+    }
+
+    volVectorField& g = tGrad();
+
+    const labelUList& owner = mesh.owner();
+    const labelUList& neighbour = mesh.neighbour();
+
+    const volVectorField& C = mesh.C();
+    const surfaceVectorField& Cf = mesh.Cf();
+
+    scalarField maxVsf(vsf.internalField());
+    scalarField minVsf(vsf.internalField());
+
+    forAll(owner, facei)
+    {
+        label own = owner[facei];
+        label nei = neighbour[facei];
+
+        scalar vsfOwn = vsf[own];
+        scalar vsfNei = vsf[nei];
+
+        maxVsf[own] = max(maxVsf[own], vsfNei);
+        minVsf[own] = min(minVsf[own], vsfNei);
+
+        maxVsf[nei] = max(maxVsf[nei], vsfOwn);
+        minVsf[nei] = min(minVsf[nei], vsfOwn);
+    }
+
+
+    const volScalarField::GeometricBoundaryField& bsf = vsf.boundaryField();
+
+    forAll(bsf, patchi)
+    {
+        const fvPatchScalarField& psf = bsf[patchi];
+
+        const labelUList& pOwner = mesh.boundary()[patchi].faceCells();
+
+        if (psf.coupled())
+        {
+            const scalarField psfNei(psf.patchNeighbourField());
+
+            forAll(pOwner, pFacei)
+            {
+                label own = pOwner[pFacei];
+                scalar vsfNei = psfNei[pFacei];
+
+                maxVsf[own] = max(maxVsf[own], vsfNei);
+                minVsf[own] = min(minVsf[own], vsfNei);
+            }
+        }
+        else
+        {
+            forAll(pOwner, pFacei)
+            {
+                label own = pOwner[pFacei];
+                scalar vsfNei = psf[pFacei];
+
+                maxVsf[own] = max(maxVsf[own], vsfNei);
+                minVsf[own] = min(minVsf[own], vsfNei);
+            }
+        }
+    }
+
+    maxVsf -= vsf;
+    minVsf -= vsf;
+
+    // create limiter
+    scalarField limiter(vsf.internalField().size(), 1.0);
+
+    forAll(owner, facei)
+    {
+        label own = owner[facei];
+        label nei = neighbour[facei];
+
+        // owner side
+        limitFace
+        (
+            limiter[own],
+            maxVsf[own],
+            minVsf[own],
+            (Cf[facei] - C[own]) & g[own]
+        );
+
+        // neighbour side
+        limitFace
+        (
+            limiter[nei],
+            maxVsf[nei],
+            minVsf[nei],
+            (Cf[facei] - C[nei]) & g[nei]
+        );
+    }
+
+    forAll(bsf, patchi)
+    {
+        const labelUList& pOwner = mesh.boundary()[patchi].faceCells();
+        const vectorField& pCf = Cf.boundaryField()[patchi];
+
+        forAll(pOwner, pFacei)
+        {
+            label own = pOwner[pFacei];
+
+            limitFace
+            (
+                limiter[own],
+                maxVsf[own],
+                minVsf[own],
+                (pCf[pFacei] - C[own]) & g[own]
+            );
+        }
+    }
+
+    if (fv::debug)
+    {
+        Info<< "gradient limiter for: " << vsf.name()
+            << " max = " << gMax(limiter)
+            << " min = " << gMin(limiter)
+            << " average: " << gAverage(limiter) << endl;
+    }
+
+    g.internalField() *= limiter;
+    g.correctBoundaryConditions();
+    gaussGrad<scalar>::correctBoundaryConditions(vsf, g);
+
+    return tGrad;
+}
+
+
+// ************************************************************************* //
