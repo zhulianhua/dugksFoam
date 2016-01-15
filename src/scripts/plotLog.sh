93727ae515324c6ea49979654d9965f867ac9809
Lianhua Zhu <zhulianhua121@gmail.com>
2016-01-15 10:42:58 +0800

    add plotLog.sh script for monitering

---
+++ src/scripts/plotLog.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+grep -e 'Temperature'  log |cut -d '=' -f 2 |nl >tlog.dat
+grep -e '^Density'  log |cut -d '=' -f 2 |nl >rlog.dat
+grep -e '^Velocity'  log |cut -d '=' -f 2 |nl >ulog.dat
+gnuplot <<- EOF
+    set xlabel "t"
+    set ylabel "log(Error)"
+    set logscale y
+    set term png
+    set output "log.png"
+    plot "tlog.dat" using 1:2 with l title "rho Convergence rate", \
+         "rlog.dat" using 1:2 with l title "T   Convergence rate", \
+         "ulog.dat" using 1:2 with l title "U   Convergence rate"
+EOF
+rm *log.dat
